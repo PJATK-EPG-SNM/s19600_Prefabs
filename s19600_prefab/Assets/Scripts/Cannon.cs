ef17e2b70bb7192f455ebf0e9efc6cbdc5506477
s19600 <61684591+s19600@users.noreply.github.com>
2020-04-27 11:01:59 +0200

    v.0.0.1

---
+++ s19600_prefab/Assets/Scripts/Cannon.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cannon : MonoBehaviour
+{
+    public float period = 1;
+    private float lastBallTime;
+
+    public CannonBall prefab;
+    public Transform startTransform;
+
+    public float rotationSpeed;
+    private float direction = -1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lastBallTime = 0;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lastBallTime += Time.deltaTime;
+
+        if (lastBallTime >= period)
+        {
+            lastBallTime = 0;
+            Instantiate(prefab, startTransform.position, startTransform.rotation);
+        }
+
+        if(lastBallTime > period/2)
+        {
+            float rotationValue = rotationSpeed * direction * Time.deltaTime;
+            Vector3 rotateVector = new Vector3(0,0, rotationValue);
+            startTransform.Rotate(rotateVector);
+
+            if(startTransform.rotation.z < -0.999 || startTransform.rotation.z >0)
+            {
+                direction *= -1;
+            }
+        }
+    }
+}
---
+++ s19600_prefab/Assets/Scripts/CannonBall.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonBall : MonoBehaviour
+{
+    public float speed = 5;
+    private Rigidbody2D rb;
+    
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = transform.right * -1 * speed;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+      if(collision.gameObject.CompareTag("My Player"))
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+
+}
